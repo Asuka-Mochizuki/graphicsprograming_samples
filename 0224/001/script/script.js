1f0e7ef6bdee333b0d800db5da60d8ea9e543aea
Asuka Mochizuki <v23.a.mochizuki@gmail.com>
2018-02-24 20:29:15 +0900

    0224 file add

---
+++ 0224/001/script/script.js
@@ -0,0 +1,65 @@
+
+// グローバル汚染を避けるために即時関数を使って全体を囲う
+// (function(){
+//     ...
+// })();と同じこと
+(() => {
+    let cu;  // CanvasUtil のインスタンス用
+    let gui; // Gui のインスタンス用
+
+    // ページのロードが完了したときに発火する load イベントを登録する
+    // windowに対して、addEventListenerでイベントが起こった際の処理を登録している
+    window.addEventListener('load', () => {
+        // Gui インスタンスの生成
+        gui = new Gui();
+
+        // CanvasUtil インスタンスの生成
+        cu = new CanvasUtil(document.getElementById('canvas'));
+
+        // サイズをウィンドウに揃える
+        cu.matchSize();
+
+        // 内容をクリアする
+        cu.clear();
+
+        // 色を塗る
+        cu.fill();
+    }, false);
+
+    /**
+     * canvas 2d context を操作するクラス
+     * キャメルケースとスネークケース（アンダーバーでつなげる奴）
+     * @class
+     */
+    class CanvasUtil {
+        /**
+         * @constructor
+         * コンストラクトとは、そのクラスが実体化されるタイミングで（一番最初に）内部的に呼び出されるメソッドのこと
+         * @param {HTMLCanvasElement} canvas - このインスタンスに紐付ける canvas
+         */
+        constructor(canvas){
+            this.canvas = canvas;
+            this.ctx = this.canvas.getContext('2d');
+        }
+        /**
+         * canvas 全体をクリアする
+         */
+        clear(){
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        }
+        /**
+         * canvas に矩形を fill モードで描く
+         */
+        fill(){
+            this.ctx.fillRect(0, 0, this.canvas.width / 2, this.canvas.height / 2);
+            gui.log('run fillRect');
+        }
+        /**
+         * canvas の大きさをウィンドウサイズに揃える
+         */
+        matchSize(){
+            this.canvas.width = window.innerWidth;
+            this.canvas.height = window.innerHeight;
+        }
+    }
+})();
